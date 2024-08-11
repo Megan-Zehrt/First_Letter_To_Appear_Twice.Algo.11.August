f0153a970498a2a9d3137508137ca455a06605ef
Megan Zehrt <zehrtmegan@gmail.com>
2024-08-11 00:04:51 -0500

    first commit

---
+++ algo.js
@@ -0,0 +1,31 @@
+// 2351. First Letter to Appear Twice
+
+
+
+// Given a string s consisting of lowercase English letters, return the first letter to appear twice.
+
+// Note:
+
+// A letter a appears twice before another letter b if the second occurrence of a is before the second occurrence of b.
+// s will contain at least one letter that appears twice.
+
+
+
+
+/**
+ * @param {string} s
+ * @return {character}
+ */
+var repeatedCharacter = function(s) {
+    
+    let map = new Map()
+
+    for(let i = 0; i < s.length; i++){
+
+        if(map.has(s[i])){
+            return s[i]
+        }else{
+            map.set(s[i], 1)
+        }
+    }
+};
